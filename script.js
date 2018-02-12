edc85830dd38f9b666c522ece0dbade355fdd748
Robert Nowaczyk <iro8ert@o2.pl>
2018-02-12 12:36:04 +0100

    remove one loop

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var a = document.getElementsByClassName('button');
 
-for (var i = 1; i <= a.length; i++) {
-  for (var n = 0; n <= a.length; n++){ 
-  console.log(a[n].innerText);
-  }
+for (var i = 0; i <= a.length; i++) {
+	console.log(a[i].innerText);
 }
